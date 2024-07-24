a9f46a85014a30b513b353276f81eb197c79de2e
Erfan <erfanhosseinizadeh@gmail.com>
2024-07-24 15:10:26 +0330

    v2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/:url", async (req, res) => {
       "vless://89b3cbba-e6ac-485a-9481-976a0415eab9@www.speedtest.net:443?encryption=none&security=tls&sni=BPB.iAmErFan.ir&alpn=h2%2Chttp%2F1.1&fp=randomized&type=ws&host=BpB.IAMErFAN.iR&path=%2FYBaAdTvWZDnAbEuM%3Fed%3D2560#%F0%9F%92%A6%20BPB%20-%20Domain_2%20%3A%20443",
       "vless://89b3cbba-e6ac-485a-9481-976a0415eab9@188.114.96.3:443?encryption=none&security=tls&sni=BPB.iAmeRFan.IR&alpn=h2%2Chttp%2F1.1&fp=randomized&type=ws&host=Bpb.IaMerfaN.Ir&path=%2F0R2BErQNzKb5NHd5%3Fed%3D2560#%F0%9F%92%A6%20BPB%20-%20IPv4_1%20%3A%20443",
       "vless://89b3cbba-e6ac-485a-9481-976a0415eab9@188.114.97.3:443?encryption=none&security=tls&sni=BPB.iAmERfan.Ir&alpn=h2%2Chttp%2F1.1&fp=randomized&type=ws&host=BPb.IAMERFAn.Ir&path=%2FyeYDz5XrFYLNePI4%3Fed%3D2560#%F0%9F%92%A6%20BPB%20-%20IPv4_2%20%3A%20443",
-      "vless://89b3cbba-e6ac-485a-9481-976a0415eab9@104.18.52.155:443?encryption=none&security=tls&sni=BPb.IAmErfAn.iR&alpn=h2%2Chttp%2F1.1&fp=randomized&type=ws&host=bPB.IaMERfAn.Ir&path=%2FqHWbKrAphGAyuar6%3Fed%3D2560#%F0%9F%92%A6%20BPB%20-%20Clean%20IP_1%20%3A%20443",
+      "vless://89b3cbba-e6ac-485a-9481-976a0415eab9@104.16.13.139:443?encryption=none&security=tls&sni=BPb.IAmErfAn.iR&alpn=h2%2Chttp%2F1.1&fp=randomized&type=ws&host=bPB.IaMERfAn.Ir&path=%2FqHWbKrAphGAyuar6%3Fed%3D2560#%F0%9F%92%A6%20BPB%20-%20Clean%20IP_1%20%3A%20443",
     ];
 
     const link1 = `https://iamerfan.ir/h8fK6YW30DpswBcb9IqMmIU/${url}/auto`;
