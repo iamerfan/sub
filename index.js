31756020349e565e6b325facbabce29a1d4ff78a
Erfan <erfanhosseinizadeh@gmail.com>
2024-09-20 02:18:15 +0330

    nl fix

--- index.js
+++ index.js
@@ -29,6 +29,31 @@ app.get("/:url", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+app.get("/sub/:url", async (req, res) => {
+  const url = req.params.url;
+  try {
+    const iamerfan = `https://iamerfan.ir/h8fK6YW30DpswBcb9IqMmIU/${url}/singbox`;
+    const nl = `https://nl.iamerfan.ir/h8fK6YW30DpswBcb9IqMmIU/${url}/singbox`;
+    if (!url) {
+      return res.status(400).send("Subscription URL parameter is missing");
+    }
+
+    // Make requests to both links concurrently
+    const [response1, response2] = await axios.all([
+      axios.get(iamerfan),
+      axios.get(nl),
+    ]);
+    const iamerfanObj = response1.data;
+    const nlObj = response2.data;
+    const lastThreeOutbounds = iamerfanObj.outbounds.slice(-3);
+    nlObj.outbounds = nlObj.outbounds.concat(lastThreeOutbounds);
+
+    res.send(nlObj);
+  } catch (error) {
+    console.error("Error fetching V2Ray subscription:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 // Start the server
 
