e4eaed01b5f3477d413bfe05c95b108dc900c613
Erfan <erfanhosseinizadeh@gmail.com>
2024-09-20 01:42:47 +0330

    return nl

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ const PORT = process.env.PORT || 3000;
 // Endpoint to fetch V2Ray subscription link
 app.get("/:url", async (req, res) => {
   const url = req.params.url;
-  if (!url) {
-    return res.status(400).send("Subscription URL parameter is missing");
-  }
-
   try {
     const link1 = `https://iamerfan.ir/h8fK6YW30DpswBcb9IqMmIU/${url}/auto`;
+    const link2 = `https://nl.iamerfan.ir/h8fK6YW30DpswBcb9IqMmIU/${url}/auto`;
+    if (!url) {
+      return res.status(400).send("Subscription URL parameter is missing");
+    }
 
-    // Fetch the main link
-    const response1 = await axios.get(link1);
+    // Make requests to both links concurrently
+    const [response1] = await axios.all([axios.get(link1)]);
     const data1 = response1.data;
+    const mixedData = `${data1}`;
 
-    res.send(data1);
+    res.send(mixedData);
   } catch (error) {
     console.error("Error fetching V2Ray subscription:", error.message);
     res.status(500).send("Internal Server Error");
@@ -26,6 +27,7 @@ app.get("/:url", async (req, res) => {
 });
 
 // Start the server
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
