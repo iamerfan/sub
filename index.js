416959b44be7535168f4e96695a212f4bd95a3fd
Erfan <erfanhosseinizadeh@gmail.com>
2024-10-03 15:45:18 +0330

    hiddify change

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.get("/sub/:url", async (req, res) => {
